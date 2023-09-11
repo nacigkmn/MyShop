077d4dea1a838554fe4566aa4f0b7decbb9e86ef
Naci Gokmen <nacigokmen99@gmail.com>
2023-09-11 11:55:45 +0200

    ItemController added

---
+++ MyShop/Controllers/ItemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+
+namespace MyShop.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Table()
+        {
+
+            var items = new List<Item>();
+
+            return View();
+        }
+    }
+}
