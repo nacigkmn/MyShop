badd6e3d269b4304876d42354e30c714f26e9895
Naci Gokmen <nacigokmen99@gmail.com>
2023-09-11 11:21:05 +0200

    Item properties added.

--- MyShop/Models/Item.cs
+++ MyShop/Models/Item.cs
@@ -2,5 +2,13 @@
 {
     public class Item
     {
+        public int ItemId { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+
+
     }
 }
