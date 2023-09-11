24448c7323a3a1379a97cfe363334b7ee6f0df7c
Naci Gokmen <nacigokmen99@gmail.com>
2023-09-11 12:13:16 +0200

    demo1 finished

--- MyShop/Controllers/ItemController.cs
+++ MyShop/Controllers/ItemController.cs
@@ -10,7 +10,24 @@ namespace MyShop.Controllers
 
             var items = new List<Item>();
 
-            return View();
+            var item1 = new Item();
+            item1.ItemId = 1;
+            item1.Name = "Pizza";
+            item1.Price = 100;
+
+            var item2 = new Item
+            {
+              ItemId = 2,
+              Name = "Turkish kebap",
+              Price = 100,
+            };
+
+            items.Add(item1);
+            items.Add(item2);
+
+            ViewBag.CurrentViewName = "List of Shop Items";
+
+            return View(items);
         }
     }
 }
--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -1,15 +1,10 @@
-namespace MyShop
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+
+        
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapDefaultControllerRoute();
 
             app.Run();
-        }
-    }
-}
